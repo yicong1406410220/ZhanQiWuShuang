384d83f0e05d13f02bcc2fb3d7c20791cdc485eb
luanshi <azazaz123456@foxmail.com>
2018-11-28 17:25:52 +0800

    dotween添加

--- Assets/UIFramework/UFScripts/root.cs
+++ Assets/UIFramework/UFScripts/root.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class root : MonoBehaviour {
+
+    
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        PanelMgr.instance.OpenPanel<TitlePanel>("");
+    }
 	
 	// Update is called once per frame
 	void Update () {
